bdfa304b62a8115fb504656d34b78073bfbe04c8
LevKulikov19 <levkulikov13@gmail.com>
2022-06-07 11:54:52 +0300

    init prototype

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,1 @@
-import '../css/videoplayer-js.scss';
-import '../../index.html';
-
-class VideoPlayer
-{
-    conteiner;
-    constructor (id){
-        conteiner = document.getElementById(id);
-    }
-}
-
-video = VideoPlayer('video');
+import './videoplayer.js';
